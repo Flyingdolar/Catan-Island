85d8a3d32ef925b4147736c9f7631064976a05af
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-05-31 13:52:09 +0800

    chore[structs.h]: Adjust include content

--- inc/structs.h
+++ inc/structs.h
@@ -1,9 +1,5 @@
 #pragma once
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "../code/catan.h"
 
 enum {
     NONE = -1,  // 無 (用於 src, user, cType)

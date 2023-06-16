2109d74b11f223916f6dfe253bba9d0e98be3755
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-06-17 07:28:39 +0800

    feat: Main Structure

--- code/main.c
+++ code/main.c
@@ -1,19 +1,53 @@
 #include "catan.h"
 
-int main() {
-    SDL_Window *win = SDL_CreateWindow("SDL2 Window", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (win == NULL) {
-        SDL_Quit();
-        return 1;
-    }
-    // 直到使用者按下 QUIT 才結束程式
-    SDL_Event e;
-    while (SDL_WaitEvent(&e)) {
-        if (e.type == SDL_QUIT)
-            break;
-    }
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+int playGame(int isNewGame) {
+    if (isNewGame > 0) init();
 
+    // 直到分出勝負前，遊戲都不會結束
+    while (1) {
+        if (game->state == END) break;  // 有玩家達成勝利條件，遊戲結束
+        switch (game->state) {
+            case SETTLE:  // 首輪回合，放置兩個村莊
+                PRINTL("遊戲開始，請依序放置兩個村莊");
+                settleAction();
+                break;
+            case DICE:  // 每回合的骰子行動
+                PRINTL("輪到 %d 骰子行動", game->turn);
+                diceAction();
+                break;
+            case BUILD:  // 每回合的建設行動
+                PRINTL("輪到 %d 建設行動", game->turn);
+                buildAction();
+                break;
+            case ROBBER:  // 每回合的強盜行動
+                PRINTL("輪到 %d 強盜行動", game->turn);
+                robberAction();
+                break;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // TODO: 開啟 SDL 視窗，並初始化
+    //
+    // TODO: 開啟 Menu 視窗，顯示選項：開始遊戲、載入遊戲、離開遊戲
+    //
+    int userChoice = 1;
+    while (userChoice != 0) {
+        // TODO: 從圖形化介面視窗獲取使用者選擇
+
+        switch (userChoice) {
+            case 1:  // 開始新遊戲
+                playGame(1);
+                break;
+            case 2:  // 載入遊戲
+                playGame(0);
+            default:
+                break;
+        }
+    }
+
+    // TODO: 關閉 SDL 視窗，並釋放相關資源
+    //
     return 0;
 }

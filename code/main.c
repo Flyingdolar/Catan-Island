1bbf1a7a5468456ee7fcc512409e005747490b6c
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-06-17 18:42:15 +0800

    fix: Set game function return value to 0

--- code/main.c
+++ code/main.c
@@ -13,6 +13,9 @@ int playGame(int isNewGame) {
     while (1) {
         if (game->state == END) break;  // 有玩家達成勝利條件，遊戲結束
         switch (game->state) {
+            case START:
+                PRINTD("遊戲尚未初始化，發生錯誤");
+                return -1;
             case SETTLE:  // 首輪回合，放置兩個村莊
                 PRINTL("遊戲開始，請依序放置兩個村莊");
                 settleAction();
@@ -29,8 +32,13 @@ int playGame(int isNewGame) {
                 PRINTL("輪到 %d 強盜行動", game->turn);
                 robberAction();
                 break;
+            default:
+                break;
         }
     }
+    // TODO: 顯示遊戲最終由誰獲勝
+    //
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
